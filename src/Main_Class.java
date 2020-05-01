6ee994e57ff55b850f9198ce1137b8a5e2ed48b2
Rohan <rst170000@utdallas.edu>
2020-05-01 11:59:00 -0500

    updated main and quicksorter

--- src/Main_Class.java
+++ src/Main_Class.java
@@ -1,9 +1,17 @@
-public class main {
+public class Main_Class {
     public static void main(String[] args) {
         int size = Integer.decode(args[0]);
-        
+        String report_file = args[1];
+        String unsorted_file = args[2];
+        String sorted_file = args[3];
+
+        QuickSorter<Integer> quicksort = new QuickSorter<Integer>();
+
 
         System.out.println(size + 1);
+        System.out.println(report_file);
+        System.out.println(unsorted_file);
+        System.out.println(sorted_file);
 
 
     }
--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -1,11 +1,18 @@
 import java.time.Duration;
 import java.lang.*;
+import java.util.ArrayList;
 
 public class QuickSorter<E> {
     public static <E extends Comparable<E>> Duration timesQuickSort(){
         long initial_time =
     }
+
+    public static ArrayList<Integer> generateRandom(int size){
+        ArrayList<Integer> random_unsorted = null;
+
+        return random_unsorted;
+    }
+
 }
 
 
-public
