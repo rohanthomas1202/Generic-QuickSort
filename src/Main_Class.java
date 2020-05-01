7c7766ceaa91f51f502dbebeca0fe1ec3c66db2d
Rohan <rst170000@utdallas.edu>
2020-05-01 16:58:42 -0500

    updated quicksort and main functions

--- src/Main_Class.java
+++ src/Main_Class.java
@@ -1,18 +1,70 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+
 public class Main_Class {
     public static void main(String[] args) {
-        int size = Integer.decode(args[0]);
-        String report_file = args[1];
-        String unsorted_file = args[2];
-        String sorted_file = args[3];
 
-        QuickSorter<Integer> quicksort = new QuickSorter<Integer>();
+        try {
+            int size = Integer.decode(args[0]);
+            Duration FIRST_ELEMENT, RANDOM_ELEMENT, MEDIAN_OF_THREE_RANDOM_ELEMENTS, MEDIAN_OF_THREE_ELEMENTS;
+
+            String report_file_name = args[1];
+            File report_file = new File(report_file_name);
+            if (!report_file.exists())
+                report_file.createNewFile();
+            FileWriter report = new FileWriter(report_file);
 
 
-        System.out.println(size + 1);
-        System.out.println(report_file);
-        System.out.println(unsorted_file);
-        System.out.println(sorted_file);
+            String unsorted_file_name = args[2];
+            File unsorted_file = new File(unsorted_file_name);
+            if (!unsorted_file.exists())
+                unsorted_file.createNewFile();
+            FileWriter unsorted = new FileWriter(unsorted_file);
 
+
+            String sorted_file_name = args[3];
+            File sorted_file = new File(sorted_file_name);
+            if (!sorted_file.exists())
+                sorted_file.createNewFile();
+            FileWriter sorted = new FileWriter(sorted_file);
+
+            ArrayList<Integer> list;
+
+            list = QuickSorter.generateRandom(size);
+
+
+            // writing values to the unsorted file
+            for (Integer integer : list) {
+                unsorted.write(integer + " ");
+            }
+
+            FIRST_ELEMENT = QuickSorter.timesQuickSort(list, QuickSorter.PivotStrategy.FIRST_ELEMENT);
+            RANDOM_ELEMENT = QuickSorter.timesQuickSort(list, QuickSorter.PivotStrategy.RANDOM_ELEMENT);
+            MEDIAN_OF_THREE_RANDOM_ELEMENTS = QuickSorter.timesQuickSort(list, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_RANDOM_ELEMENTS);
+            MEDIAN_OF_THREE_ELEMENTS = QuickSorter.timesQuickSort(list, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_ELEMENTS);
+
+            for (Integer integer : list) {
+                sorted.write(integer + " ");
+            }
+
+
+
+            report.write("Array Size = " + size + "\n");
+            report.write("FIRST_ELEMENT : " + FIRST_ELEMENT + "\n");
+            report.write("RANDOM_ELEMENT : " + RANDOM_ELEMENT + "\n");
+            report.write("MEDIAN_OF_THREE_RANDOM _ELEMENTS : " + MEDIAN_OF_THREE_RANDOM_ELEMENTS + "\n");
+            report.write("MEDIAN_OF_THREE_ELEMENTS : " + MEDIAN_OF_THREE_ELEMENTS + "\n");
+
+            report.close();
+            unsorted.close();
+            sorted.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -1,18 +1,57 @@
 import java.time.Duration;
-import java.lang.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class QuickSorter<E> {
-    public static <E extends Comparable<E>> Duration timesQuickSort(){
-        long initial_time =
+    public static <E extends Comparable<E>> Duration timesQuickSort(ArrayList<E> list, PivotStrategy pivotStrategy) {
+        long startTime = System.nanoTime();
+
+
+        if (list != null && pivotStrategy != null) {
+            Collections.sort(list);
+        } else if (list == null) {
+            throw new NullPointerException("list is null");
+        } else{
+            throw new NullPointerException("not a valid pivot strategy");
+        }
+
+
+        return Duration.ofNanos((System.nanoTime() - startTime));
     }
 
-    public static ArrayList<Integer> generateRandom(int size){
-        ArrayList<Integer> random_unsorted = null;
+    public static ArrayList<Integer> generateRandom(int size) {
+        // checking if the size is positive
+        if (size > 0) {
+            long initial_time = System.nanoTime();
 
-        return random_unsorted;
+            // creating a new arraylist
+            ArrayList<Integer> random_unsorted = new ArrayList<>(size);
+
+            // populating the arraylist with values in order so that we do not get duplicates
+            for (int i = 0; i <= size; i++) {
+                random_unsorted.add(i + 1);
+            }
+            // un-sorting the values in the ArrayList
+            Collections.shuffle(random_unsorted);
+            Collections.shuffle(random_unsorted);
+            System.out.println("Time taken is " + (System.nanoTime() - initial_time));
+            // returning the un-sorted value
+            return random_unsorted;
+        } else if (size < 0) {
+            // if size is negative
+            throw new IllegalArgumentException("size is negative");
+        } else {
+            // if size is zero
+            throw new IllegalArgumentException("size is zero");
+        }
+
+    }
+
+    public enum PivotStrategy {
+        FIRST_ELEMENT,
+        RANDOM_ELEMENT,
+        MEDIAN_OF_THREE_RANDOM_ELEMENTS,
+        MEDIAN_OF_THREE_ELEMENTS,
     }
 
 }
-
-
