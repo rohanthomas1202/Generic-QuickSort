ed400b6b8d724f84601373e2358482b84dd67d9a
Rohan <rst170000@utdallas.edu>
2020-05-01 11:36:57 -0500

    initial commit

---
+++ src/QuickSorter.java
@@ -0,0 +1,11 @@
+import java.time.Duration;
+import java.lang.*;
+
+public class QuickSorter<E> {
+    public static <E extends Comparable<E>> Duration timesQuickSort(){
+        long initial_time =
+    }
+}
+
+
+public
